f60dba30e4d8f7c225d9e2c4dcf1cb8091c2ef8e
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-18 11:43:23 +0700

    feat:add users migration

---
+++ migrations/20260818040802-create-users.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize";
+
+export async function up(queryInterface) {
+  await queryInterface.createTable("users", {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+
+    password_hash: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+
+    avatar_url: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
+
+export async function down(queryInterface) {
+  await queryInterface.dropTable("users");
+}
